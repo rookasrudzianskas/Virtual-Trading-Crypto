8ad754d0f984dd4c4f411903c79aeafe59f7d593
Rokas <rookas.rudzenskas@gmail.com>
2021-08-09 13:13:26 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import {Text} from "react-native";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
